cf1dd9a4c04c6a4880d7beffdbefc302939a67ec
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-04-09 12:16:00 +0700

    add repository

--- app/src/main/java/com/chris/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/chris/wishlistapp/WishViewModel.kt
@@ -4,8 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chris.wishlistapp.data.Wish
+import com.chris.wishlistapp.data.WishRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
-class WishViewModel: ViewModel() {
+
+class WishViewModel(
+    private val wishRepository: WishRepository
+): ViewModel() {
 
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
@@ -16,5 +25,40 @@ class WishViewModel: ViewModel() {
 
     fun wishDescriptionChanges(newString: String) {
         wishDescriptionState = newString
+    }
+
+    // using lateinit means that it will set "something" up before we need it, example when using
+    // flow or coroutines
+
+    lateinit var getAllWishes: Flow<List<Wish>>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+
+
+    // Using Dispatcher to optimize, IO stands for Input-Output
+    // Efficient managing the threads
+    fun addWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.addWish(wish)
+        }
+    }
+
+    fun getAWishById(id: Long) : Flow<Wish> {
+        return wishRepository.getAWishById(id)
+    }
+    fun updateWish(wish: Wish) {
+        viewModelScope.launch (Dispatchers.IO) {
+            wishRepository.updateWish(wish)
+        }
+    }
+
+    fun deleteWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.deleteWish(wish)
+        }
     }
 }
---
+++ app/src/main/java/com/chris/wishlistapp/data/WishRepository.kt
@@ -0,0 +1,25 @@
+package com.chris.wishlistapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class WishRepository(private val wishDao: WishDao) {
+    suspend fun addWish(wish: Wish){
+        wishDao.addWish(wish)
+    }
+
+    fun getWishes() : Flow<List<Wish>> = wishDao.getAllWishes()
+
+    fun getAWishById(id: Long) : Flow<Wish> {
+        return wishDao.getAWishById(id)
+    }
+
+    suspend fun updateWish(wish: Wish) {
+        wishDao.updateAWish(wish)
+    }
+
+    suspend fun deleteWish(wish: Wish) {
+        wishDao.deleteAWish(wish)
+    }
+
+
+}
